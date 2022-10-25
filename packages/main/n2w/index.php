055fb0da4fa7db5f5cacff737ba66ab5c5c652cb
ivorhill <ivorhill@outlook.com>
2022-10-25 16:29:37 +0100

    renamed

--- packages/main/n2w/index.php
+++ packages/main/n2w/index.php
@@ -1,21 +1,26 @@
 <?php
+
 use NFNumberToWord\NumberToWords;
- 
-function main(array $args) : array
+
+function main(array $args): array
 {
     if (!isset($args['number'])) {
         return wrap(['error' => 'Please supply a number.']);
     }
-    
+
     $number = (int)($args['number']);
     $words = (new NumberToWords)->toWords($number);
- 
+
+    // return [
+    //     'body' => $words,
+    // ];
+
     return [
-        'body' => $words,
+        'body' => "testr",
     ];
 }
 
-function wrap(array $args) : array
+function wrap(array $args): array
 {
     return ["body" => $args];
 }
